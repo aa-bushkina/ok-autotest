74b364c1f8cf9e484654c0ed0232b54f26704886
shumi_kd2r0n5 <shumilov.iv@mail.ru>
2022-10-25 20:02:26 +0300

    created UserPage

--- src/main/java/ok/practice/pages/UserPage.java
+++ src/main/java/ok/practice/pages/UserPage.java
@@ -1,2 +1,19 @@
-package ok.practice.pages;public class UserPage {
+package ok.practice.pages;
+
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selectors.byClassName;
+import static com.codeborne.selenide.Selectors.byXpath;
+
+public class UserPage {
+
+    private static SelenideElement dropMenu = $(byClassName("u-menu_a toggle-dropdown"));
+    private static SelenideElement givePresentBtn =
+            $(byXpath("//a[@class = \"u-menu_a \" and contains(text(), \"подарок\")]"));
+
+    public void pressGiftBtn () {
+        dropMenu.click();
+        givePresentBtn.click();
+    }
 }
