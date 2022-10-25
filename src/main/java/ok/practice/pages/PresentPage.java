8c8d1ff3ed21bba665c57f1dfd1cfc76419db810
Alena Bushkina <aa-bushkina@mail.ru>
2022-10-25 21:46:08 +0300

    fix another locators

--- src/main/java/ok/practice/pages/PresentPage.java
+++ src/main/java/ok/practice/pages/PresentPage.java
@@ -1,6 +1,7 @@
 package ok.practice.pages;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
 import dev.failsafe.internal.util.Assert;
 
 import org.openqa.selenium.By;
@@ -11,7 +12,7 @@ import static com.codeborne.selenide.Selenide.$;
 public class PresentPage
 {
   private static final By PRESENT_BUTTON = byXpath("//*[@data-l='ti,923978881408,t,present']");
-  private static final By BY_BUTTON = byXpath("//button[@class = 'js-submit-to-friend button-pro __ic']");
+  private static final By BY_BUTTON = byXpath("//*[contains(text(),'Подарить за 0 ОК')]");
   private static final By MESSAGE = byXpath("//*[contains(@class,'tico')]");
 
   public String getGiftMsg()
