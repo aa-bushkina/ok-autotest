c8a22473801276db827d8da6319982e545c503c1
Alena Bushkina <aa-bushkina@mail.ru>
2022-10-25 20:02:18 +0300

    make logIn in test

---
+++ src/main/java/ok/practice/pages/TestsData.java
@@ -0,0 +1,8 @@
+package ok.practice.pages;
+
+public class TestsData
+{
+  final public String LogInUrl = "https://ok.ru/";
+  final public String ValidUsername = "technoPol5";
+  final public String ValidPassword = "technoPolis2022";
+}
