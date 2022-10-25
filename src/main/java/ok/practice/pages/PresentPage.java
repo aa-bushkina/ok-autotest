fa57b9052553400caf5827efdd8515af0f3ecee9
Ruslana <ruslana.danilova@vk.team>
2022-10-25 20:11:03 +0300

    Created PresentPage

---
+++ src/main/java/ok/practice/pages/PresentPage.java
@@ -0,0 +1,17 @@
+package ok.practice.pages;
+
+import com.codeborne.selenide.Condition;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selectors.byXpath;
+import static com.codeborne.selenide.Selenide.$;
+
+public class PresentPage {
+    private static final By PRESENT_BUTTON = byXpath("//a[@data-l='ti,923978881408,t,present']");
+    private static final By BY_BUTTON = byXpath("//a[@class='js-submit-to-friend button-pro __ic']");
+
+    public void present () {
+        $(PRESENT_BUTTON).click();
+        $(BY_BUTTON).click();
+    }
+}
